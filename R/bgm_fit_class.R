8ef8031480a619a3800ea26a7ed6ff4a4943ea7b
KarolineHuth <karoline.huth@gmail.com>
2023-09-28 07:50:43 -0700

    CRAN fixes round 2

--- R/bgm_fit_class.R
+++ R/bgm_fit_class.R
@@ -1,11 +1,11 @@
 #' Fit a Bayesian analysis of networks
 #'
-#' @param x Object with a particular class that will dispatch to the respective package functions
+#' @param fit Object with a particular class that will dispatch to the respective package functions
 #' @param ... Additional arguments to be passed onto the respective fitting functions
 
 
-bgm_fit <- function(x, ...){
+bgm_fit <- function(fit, ...){
 
-  UseMethod("bgm_fit", x)
+  UseMethod("bgm_fit", fit)
 
 }
